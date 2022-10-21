73f4f8d5b7a7600d6bb13692e86da730f9cce476
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-10-21 16:43:17 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import { Component } from 'react';
 import Header from './components/Header';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <Header />

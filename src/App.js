3ab9eeb846cc97f407ff06d99fa36391e807ceb6
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-10-21 19:41:39 -0300

    Requisite 10 complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Component } from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
-import MissionCard from './components/MissionCard';
 
 class App extends Component {
   render() {
@@ -11,7 +10,6 @@ class App extends Component {
         <Header />
         <SolarSystem />
         <Missions />
-        <MissionCard />
       </>
     );
   }

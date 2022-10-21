0b6c05b58e6b025201f5eb8c6b50f76da05d60b1
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-10-21 16:15:47 -0300

    Requisite 1 complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import { Component } from 'react';
+import Header from './components/Header';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <Header />
+    );
   }
 }
 

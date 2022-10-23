5364169a4dd81d36074ba3ab1327209a4fce009b
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-10-22 22:12:04 -0300

    Header CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { Component } from 'react';
+import '../Assets/Styles/Header.css';
 
 class Header extends Component {
   render() {
     return (
-      <header>
+      <header className="Header">
         <h1>Sistema Solar</h1>
+        <span className="elipse"> </span>
       </header>
     );
   }
